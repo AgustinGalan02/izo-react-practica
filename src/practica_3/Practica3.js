7f7a85034fd4331acb7df460182cfbdae517abb9
AgusG <agustin.galan@institutozonaoeste.edu.ar>
2023-09-26 14:27:58 -0300

    26-9

--- src/practica_3/Practica3.js
+++ src/practica_3/Practica3.js
@@ -1,21 +1,34 @@
 import { useState } from "react";
 
-
-export default function Practica3 ({ props }) { 
+export default function Practica3({ props }) {
     const [arr, setArr] = useState([]);
+
+    const [valorCajaTexto, setValorCajaTexto] = useState('');
+    const onCajaTextoChange = (event) => {
+        setValorCajaTexto(event.target.value);
+    }
+
     const onClickHandler = () => {
-        // let newArr = [...arr];
-        let nArr = [...arr, 'x'];
+        let nArr = [...arr, valorCajaTexto];
         setArr(nArr);
     }
     return (
         <div>
             <h3>Práctica 3</h3>
-            <input type="text" />
+            <input type="text" value={valorCajaTexto} onChange={onCajaTextoChange} />
+
             <button onClick={onClickHandler}>Agregar</button>
-            { arr.map((item) => (
-                <div>{item}</div>
-            ))}
+            <ul>
+                {arr.map((numero) => (
+                    <li>
+                        { !isNaN(numero) && <div>{numero} - {numero % 2 === 0 ? 'PAR' : 'IMPAR'}</div> }
+                        <input type="text" value={isNaN(numero) && {numero}}/> {numero} 
+                        { isNaN(numero) && <div style={{marginLeft:"800px", marginBottom:"22px"}}><div style={{borderStyle:"double", textAlign:"center"}}>{numero}</div></div> }
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
+
+// [Contador: { counter } / { counter % 2 === 0 ? ‘(par)’ : ‘(impar)’}]
